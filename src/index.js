9e25535c8c73720f3d70140344e826306a63aaed
antiluddite <88464949+antiluddite@users.noreply.github.com>
2022-04-23 14:44:32 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+// function double(x) {
+//   return x * 2
+// }
+// const newNumbers = numbers.map(double);
+
+//for each to loop through each item in array
+const newNumbers = [];
+function double(x) {
+  newNumbers.push(x * 2);
+}
+numbers.forEach(double);
+
+console.log(newNumbers);
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
